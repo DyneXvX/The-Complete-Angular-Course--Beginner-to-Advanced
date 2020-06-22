1f8ce2bce2aba8eb0415ba42d1756edc107c907d
Justin Thoms <DyneXvX@outlook.com>
2020-06-22 15:41:38 -0400

    End of Fundamentals Section 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -16,7 +17,9 @@ import { CourseComponent } from './course/course.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ //this is where we put all the dependencies.
+    CoursesService
+  ],           
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,10 +1,34 @@
+import { CoursesService } from './courses.service';
 
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses<h2>'    
+    //template: '<h2>{{ "Title: " + title }}<h2>'    
+    //template: '<h2>{{ getTitle() }}<h2>' //this type of syntax is called string Interpolation
+    template: `<h2>{{ getTitle() }}<h2>
+                <ul>
+                    <li *ngFor="let course of courses">
+                        {{ course }}
+                    </li>
+                </ul>
+                `
 })
-export class CoursesComponent{
 
+export class CoursesComponent{
+title ="List of courses";
+courses;
+
+constructor(service: CoursesService){               //<--lossely coupled instead of let below.
+    //let service = new CoursesService();
+    //dependceing injection is next.. This won't work alone. 
+    //2nd. Went to ap.module.ts and added coursesservice there. See file!
+    this.courses = service.getCourses();
+}
+
+//Logic for calling HTTP service. 
+
+getTitle(){
+    return this.title;
+}
 }
